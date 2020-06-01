a18a09596375860246bca0d30efa6950e86c5655
dante <dantefe02@gmail.com>
2020-06-01 18:35:35 -0300

    sprint 2 finalizado
    NavigationView añadido con Fragment AboutUs

--- app/src/main/java/com/example/dh_entregableandroid_danteferrari/MainActivity.java
+++ app/src/main/java/com/example/dh_entregableandroid_danteferrari/MainActivity.java
@@ -1,21 +1,48 @@
 package com.example.dh_entregableandroid_danteferrari;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity implements RecylerViewFragment.RecyclerViewFragmentListener {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    private DrawerLayout drawerLayoutMainActivity;
+    private NavigationView navigationView;
 
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        OnFindViewById();
+
         final RecylerViewFragment recylerViewFragment = new RecylerViewFragment();
+        final AboutUsFragment aboutUsFragment = new AboutUsFragment();
         pegarFragment(recylerViewFragment);
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                switch (item.getItemId()){
+                    case R.id.AboutUs:
+                        pegarFragment(aboutUsFragment);
+                }
+
+                drawerLayoutMainActivity.closeDrawers();
+                
+                return true;
+            }
+        });
     }
 
     private void pegarFragment(Fragment unFragment) {
@@ -37,4 +64,10 @@ public class MainActivity extends AppCompatActivity implements RecylerViewFragme
         pegarFragment(detailFragment);
 
     }
+
+    private void OnFindViewById() {
+        drawerLayoutMainActivity = findViewById(R.id.activityMain_DrawerLayout);
+        navigationView = findViewById(R.id.activityMain_NavigationView);
+    }
+
 }
